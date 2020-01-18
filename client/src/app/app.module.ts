3e310f3f511335205c1e2e6a45e7d0c91bc57c0b
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-18 12:24:42 +0530

    initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoService } from './todo.service';
+import { UserService } from './user.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    TodoService,
+    UserService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
